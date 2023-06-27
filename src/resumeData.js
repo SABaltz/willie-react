c7ee16d133d0ccda755803fd5f68767c4c71fb6a
sam <samuelabaltz@gmail.com>
2023-06-27 10:10:41 -0600

    Run code cleanup

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,107 +1,107 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
+    "imagebaseurl": "https://rbhatia46.github.io/",
     "name": "Willie Baltz",
     "role": "Percussionist and Composer",
-    "linkedinId":"Your LinkedIn Id",
+    "linkedinId": "Your LinkedIn Id",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
+    "socialLinks": [
         {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+            "className": "fa fa-linkedin"
         },
         {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
+            "name": "github",
+            "url": "http://github.com/rbhatia46",
+            "className": "fa fa-github"
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
+            "name": "skype",
+            "url": "http://twitter.com/rbhatia46",
+            "className": "fa fa-twitter"
         }
-      ],
-    "aboutme":"University Of Montana Alumni specalizing in percussion porformance, music eduction, and composition for media and advertisements.",
-    "address":"Columbia Falls MT",
-    "website":"https://WillieBaltz.com",
-    "phone":"(406) 253-2867",
-    "education":[
-      {
-        "UniversityName":"",
-        "specialization":"Music Performance",
-        "MonthOfPassing":"May",
-        "YearOfPassing":"2023",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"The University Of Montana",
-        "specialization":"Percussion Performance",
-        "MonthOfPassing":"",
-        "YearOfPassing":"2023",
-        "Achievements":""
-      }
     ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
+    "aboutme": "University Of Montana Alumni specalizing in percussion porformance, music eduction, and composition for media and advertisements.",
+    "address": "Columbia Falls MT",
+    "website": "https://WillieBaltz.com",
+    "phone": "(406) 253-2867",
+    "education": [
+        {
+            "UniversityName": "",
+            "specialization": "Music Performance",
+            "MonthOfPassing": "May",
+            "YearOfPassing": "2023",
+            "Achievements": "Some Achievements"
+        },
+        {
+            "UniversityName": "The University Of Montana",
+            "specialization": "Percussion Performance",
+            "MonthOfPassing": "",
+            "YearOfPassing": "2023",
+            "Achievements": ""
+        }
     ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"Percussion"
-      },
-      {
-        "skillname":"Logic Pro X"
-      },
-      {
-        "skillname":"Steel Drums"
-      }
+    "work": [
+        {
+            "CompanyName": "Some Company",
+            "specialization": "Some specialization",
+            "MonthOfLeaving": "Jan",
+            "YearOfLeaving": "2018",
+            "Achievements": "Some Achievements"
+        },
+        {
+            "CompanyName": "Some Company",
+            "specialization": "Some specialization",
+            "MonthOfLeaving": "Jan",
+            "YearOfLeaving": "2018",
+            "Achievements": "Some Achievements"
+        }
     ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
+    "skillsDescription": "Your skills here",
+    "skills": [
+        {
+            "skillname": "Percussion"
+        },
+        {
+            "skillname": "Logic Pro X"
+        },
+        {
+            "skillname": "Steel Drums"
+        }
     ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
+    "portfolio": [
+        {
+            "name": "project1",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/phone.jpg"
+        },
+        {
+            "name": "project2",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project.jpg"
+        },
+        {
+            "name": "project3",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project2.png"
+        },
+        {
+            "name": "project4",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/phone.jpg"
+        }
+    ],
+    "testimonials": [
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        },
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        }
     ]
-  }
-  
-  export default resumeData
+}
+
+export default resumeData
